31ef2cb18a556463421bd502bbe81994f2e00ab5
Ilya <rodionov84@yandex.ru>
2023-07-15 20:36:17 +0300

    fixing authentication failed  3.4

--- files/db.js
+++ files/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const connectPool = new Pool({
     user: "postgres",   
-    password: "Chang-React23",
+    password: "Chang",
     host: "localhost",
     port: 5432,
     database: "pernins",
