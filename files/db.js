8f4415b13b55cedf00ec7f3ae5f397b92142680d
Ilya <rodionov84@yandex.ru>
2023-07-15 19:45:16 +0300

    fixing ECONNREFUSED 2.2

--- files/db.js
+++ files/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const connectPool = new Pool({
     user: "postgres",   
-    password: "",
+    password: "Chang-React23",
     host: "80.78.248.241",
     port: 5432,
     database: "pernins",
