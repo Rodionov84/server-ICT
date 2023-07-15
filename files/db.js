44ebb6a3f0efa37f67e6d0e2cf26624445657479
Ilya <rodionov84@yandex.ru>
2023-07-15 12:40:59 +0300

    changed hostname in db.js

--- files/db.js
+++ files/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",   
     password: "Chang",
-    host: "localhost",
+    host: "http://www.react-js.site",
     port: 5432,
     database: "pernins",
 })
